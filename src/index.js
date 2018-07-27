2eb6a68059c8ef248dc83927b028c8d8edb0bd80
Bianca Simion <bianca.simion@matchesfashion.com>
2018-07-27 12:32:32 +0100

    added redux store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
+
+console.log('store.getState()', store.getState());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
 
 
