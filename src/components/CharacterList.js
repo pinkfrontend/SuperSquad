2f9b7ce42c40f671c18d42e28c0419646f2e3118
Bianca Simion <bianca.simion@matchesfashion.com>
2018-08-03 14:01:00 +0100

    added a list of characters

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -3,17 +3,26 @@ import { connect } from 'react-redux';
 
 class CharacterList extends Component {
   render() {
-    console.log('PROPS', this.props);
     return (
       <div>
         <h4>Characters</h4>
+        <ul>
+          {
+            this.props.characters.map(character => {
+              return (
+                <li key={character.id}>
+                  {character.name}
+                </li>
+              )
+            })
+          }
+        </ul>
       </div>
     )
   }
 }
 
 function mapStateToProps(state) {
-  console.log('state ---', state);
   return {
     characters: state.characters
   };
